1e199ec0a66bd12a4ec8a089c2464f4c0d5ce61b
psbvision <punitsinghbisht1529@gmail.com>
2025-01-13 11:02:22 +0530

    updated many things

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Provider from "./provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +22,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Provider>{children}</Provider>
+        {children}
       </body>
     </html>
   );
--- app/provider.jsx
+++ app/provider.jsx
@@ -1,17 +1,9 @@
-"use client";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
-const provider = (children) => {
-  return (
-    <div>
-      <NextThemesProvider
-        attribute="class"
-        defaultTheme="system"
-        
-      >
-        {children}
-      </NextThemesProvider>
-    </div>
-  );
-};
+import React from 'react'
 
-export default provider;
+const provider = () => {
+  return (
+    <div>provider</div>
+  )
+}
+
+export default provider
