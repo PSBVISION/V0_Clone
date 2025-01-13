bad9fab61255b190ed026ae04a0884e0d1720053
psbvision <punitsinghbisht1529@gmail.com>
2025-01-13 18:31:24 +0530

    adjusted something

--- components/custom/Header.jsx
+++ components/custom/Header.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 import {ModeToggle} from "./ModeToggle";
 const Header = () => {
   return (
-    <div>
+    <div className="p-4 flex justify-between items-center">
       <Image src={"/vercel.svg"} width={25} height={25} alt={"logo"}/>
+      <div className="flex gap-4">
       <ModeToggle />
+      <Button variant={"default"}>SignIN</Button>
+      <Button>Get Started</Button>
+      </div>
     </div>
   );
 };
