17e05ee106707c9640cfe05e13d33e4c4ca54cfa
psbvision <punitsinghbisht1529@gmail.com>
2025-01-24 23:13:50 +0530

    done something

--- app/provider.jsx
+++ app/provider.jsx
@@ -4,12 +4,13 @@ import Header from "@/components/custom/Header";
 import { MessagesContext } from "@/context/MessagesContext";
 import { UserDetailContext } from "@/context/UserDetailContext";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 const provider = ({ children }) => {
   const [ Messages, setMessages ] = useState();
   const [ UserDetail, setUserDetail ]  = useState();
   return (
     <div>
-      <GoogleOAuthProvider clientId="<your_client_id>"></GoogleOAuthProvider>
+      <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_AUTH_CLIENT_ID_KEY}>
       <UserDetailContext.Provider value={{UserDetail, setUserDetail}}>
       <MessagesContext.Provider value={{Messages, setMessages}}>
       <NextThemesProvider
@@ -24,7 +25,7 @@ const provider = ({ children }) => {
       </NextThemesProvider>
       </MessagesContext.Provider>
       </UserDetailContext.Provider>
-
+      </GoogleOAuthProvider>
     </div>
   );
 };
--- components/custom/LoginDialog.jsx
+++ components/custom/LoginDialog.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use Client";
+import React, { useContext } from "react";
 import Lookup from "@/data/Lookup";
 import {
   Dialog,
@@ -8,8 +9,26 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
+import { useGoogleLogin } from "@react-oauth/google"; 
+import axios from "axios";
+import { UserDetailContext } from "@/context/UserDetailContext";
 
 const LoginDialog = ({ OpenDialog, CloseDialog }) => {
+  const {UserDetail, setUserDetail} = useContext(UserDetailContext);
+  const googleLogin = useGoogleLogin({
+    onSuccess: async (tokenResponse) => {
+      console.log(tokenResponse);
+      const userInfo = await axios.get(
+        "https://www.googleapis.com/oauth2/v3/userinfo",
+        { headers: { Authorization: "Bearer" + tokenResponse?.access_token } }
+      );
+
+      console.log(userInfo);
+      setUserDetail(userInfo?.data);
+      closeDialog(false);
+    },
+    onError: (errorResponse) => console.log(errorResponse),
+  });
   return (
     <Dialog open={OpenDialog} onOpenChange={CloseDialog}>
       <DialogContent>
@@ -17,9 +36,13 @@ const LoginDialog = ({ OpenDialog, CloseDialog }) => {
           <DialogTitle></DialogTitle>
           <DialogDescription>
             <div className="flex flex-col items-center justify-center gap-3">
-              <h2 className="font-bold text-2xl text-center text-white">{Lookup.SIGNIN_HEADING}</h2>
+              <h2 className="font-bold text-2xl text-center text-white">
+                {Lookup.SIGNIN_HEADING}
+              </h2>
               <p className="mt-2 text-center">{Lookup.SIGNIN_SUBHEADING}</p>
-              <Button className="bg-blue-500 text-white hover:bg-blue-400 mt-3">Sign In with Google</Button>
+              <Button onClick={googleLogin} className="bg-blue-500 text-white hover:bg-blue-400 mt-3">
+                Sign In with Google
+              </Button>
               <p>{Lookup?.SIGNIn_AGREEMENT_TEXT}</p>
             </div>
           </DialogDescription>
