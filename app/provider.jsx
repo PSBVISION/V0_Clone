6f044d8b2e65dd289e54b7580b374a227afbe44d
psbvision <punitsinghbisht1529@gmail.com>
2025-01-30 22:40:11 +0530

    backend setup almost complete

--- app/provider.jsx
+++ app/provider.jsx
@@ -1,13 +1,30 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "@/components/custom/Header";
 import { MessagesContext } from "@/context/MessagesContext";
 import { UserDetailContext } from "@/context/UserDetailContext";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { useConvex } from "convex/react";
+import { api } from "@/convex/_generated/api";
 const provider = ({ children }) => {
   const [ Messages, setMessages ] = useState();
   const [ UserDetail, setUserDetail ]  = useState();
+  const convex=useConvex();
+  useEffect(()=>{
+    isAuthenticated();
+  },[])
+  const isAuthenticated = async() =>{
+    if(typeof window !== "undefined"){
+      const user = JSON.parse(localStorage.getItem('user'))
+      //fetch from database
+      const result = await convex.query(api.users.GetUser,{
+        email:user?.email
+      })
+      setUserDetail(result);
+      console.log(result);
+    }
+  }
   return (
     <div>
       <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_AUTH_CLIENT_ID_KEY}>
--- convex/users.js
+++ convex/users.js
@@ -1,5 +1,5 @@
 import { v } from "convex/values";
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 
 export const CreateUser=mutation({
   args:{
@@ -19,8 +19,17 @@ export const CreateUser=mutation({
         email:args.email,
         uid:args.uid
       });
-      console.log(result);
-      
+      console.log(result);    
     }
   }
 })
+
+export const GetUser = query({
+  args:{
+    email:v.string(),
+  },
+  handler:async(ctx,args)=>{
+    const user = await ctx.db.query('users').filter((q)=>q.eq(q.field('email'),args.email)).collect(); 
+    return user[0];
+  }
+}) 
